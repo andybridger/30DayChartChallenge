76207cfb91c48f9accd9fce1347edddbd16fdab0
andybridger <69238442+andybridger@users.noreply.github.com>
2021-04-07 22:14:23 +1000

    Failed R code for chart on day 3

---
+++ day03/day03.R
@@ -0,0 +1,23 @@
+#Attempted to do this chart in R but failed so went back to excel.
+#Insert line chart in R and insert line to indicate time break on the x-axis
+
+# load libraries ----
+library(data.table)
+library(tidyverse)
+library(readxl)
+library(ggplot2)
+library(aigtheme)
+
+d_int <- read_csv("data/day03.csv")
+str(d_int)
+view(d_int)
+
+ch_int <- ggplot(data=d_int, aes(x = year)) +
+  geom_line(aes(y=short_term),size = 1, color = "#1ab0d4", group = 2) +
+  geom_line(aes(y=long_term),size = 1, color = "#1a698a")+
+  aig_style()+
+  aig_y_continuous(limits=c(0,20),breaks = seq(0,20, by = 4))+
+  scale_x_discrete(year)+
+  labs(title = "5000 years of interest rates",
+       subtitle = "Short-term and long-term interest rates (%)",
+       caption = "Note: insert note")
